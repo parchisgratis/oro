f24ac2f6b341a666210fc3286da25b29863ef058
parchisgratis <97053743+parchisgratis@users.noreply.github.com>
2022-01-03 13:33:21 -0500

    start

---
+++ librerias/js/interactividad.js
@@ -0,0 +1,95 @@
+/* 
+The Creative Commons CC0 Public Domain Dedication waives copyright interest in a
+work you've created and dedicates it to the world-wide public domain. Use CC0 to
+opt out of copyright entirely and ensure your work has the widest reach. As with
+the Unlicense and typical software licenses, CC0 disclaims warranties. CC0 is 
+very similar to the Unlicense.
+    Created on : 6/01/2020, 02:59:20 PM
+    Author     : oscar gonzalez
+    Email      : oigonzalez83@misena.edu.co
+*/
+
+$(document).ready(function(){
+    $("#a0b").hide();
+    $("#a1b").hide();
+    $("#a2b").hide();
+    $("#a3b").hide();
+    $("#a4b").hide();
+    $("#a5b").hide();
+    $("#a6b").hide();
+    $("#a7b").hide();
+    $("#a8b").hide();
+
+});
+
+$(document).ready(function(){
+    $("#a0a").click(function () {
+        $("#a0b").show();
+    });
+    $("#a0b").mousedown(function () {
+        $("#a0b").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#a1a").click(function () {
+        $("#a1b").show();
+    });
+    $("#a1b").mousedown(function () {
+        $("#a1b").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#a2a").click(function () {
+        $("#a2b").show();
+    });
+    $("#a2b").mousedown(function () {
+        $("#a2b").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#a3a").click(function () {
+        $("#a3b").show();
+    });
+    $("#a3b").mousedown(function () {
+        $("#a3b").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#a4a").click(function () {
+        $("#a4b").show();
+    });
+    $("#a4b").mousedown(function () {
+        $("#a4b").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#a5a").click(function () {
+        $("#a5b").show();
+    });
+    $("#a5b").mousedown(function () {
+        $("#a5b").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#a6a").click(function () {
+        $("#a6b").show();
+    });
+    $("#a6b").mousedown(function () {
+        $("#a6b").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#a7a").click(function () {
+        $("#a7b").show();
+    });
+    $("#a7b").mousedown(function () {
+        $("#a7b").hide();
+    });
+});
